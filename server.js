d452f58c4db8d2cb3916982432acd89362ef1367
louisefindlay23 <louisefindlay23@outlook.com>
2020-05-18 16:13:11 +0100

    Added goodreads-api-node package

--- server.js
+++ server.js
@@ -2,6 +2,16 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const goodreads = require('goodreads-api-node');
+
+// Goodreads API - NodeJS
+
+const myCredentials = {
+    key: 'LDomy4VKhZXCrcE3rJ8TQ',
+    secret: ' xCaCeVJvD5G7mbfu7FgEg0nyzFKl6WK63ph4CGLQuI'
+};
+
+const gr = goodreads(myCredentials);
 
 // Initalising Express
 app.use(express.static('public'));
@@ -12,6 +22,8 @@ app.set('view engine', 'ejs');
 
 // Root Route
 app.get('/', function (req, res) {
+    gr.getBooksByAuthor('175417')
+        .then(console.log);
     res.render("pages/index");
 });
 
