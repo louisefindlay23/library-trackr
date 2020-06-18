daf526984ce59734fb80890d7d6e0a8cf380be33
louisefindlay23 <louise@louisefindlay.com>
2020-06-18 20:52:58 +0100

    Can now get user id

--- server.js
+++ server.js
@@ -146,6 +146,19 @@ app.get("/authenticate", function (req, res) {
         });
 });
 
+app.get('/request', function (req, res) {
+    gr.getAccessToken()
+        .then(() => {
+            /* you can now make authenticated requests */
+            console.log(req.user.id);
+            gr.getUserFollowings(req.user.id)
+                .then(console.log);
+        }).catch(function () {
+            console.log("Promise Rejected");
+        });
+    res.render('pages/index', {});
+});
+
 // Goodreads Login
 
 app.get('/auth/goodreads',
@@ -157,9 +170,13 @@ app.get('/auth/goodreads/callback',
         failureRedirect: '/login'
     }),
     function (req, res) {
-        res.redirect('/');
+        // res.redirect('/request');
+        console.log(req.user);
+        console.log(req.user.id);
+        res.render('pages/index', {
+            user: req.user
+        });
     });
-
 
 // *** POST Routes ***
 
